b8929d50ed91e8112c1527651c49474596b00264
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2024-06-25 16:56:28 +0800

    Use 'createPortal' to fix it.

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,12 @@
 import { useRef, forwardRef, useImperativeHandle } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal(
   { targetTime, remainTime, onReset },
   ref
 ) {
   const dialogRef = useRef();
+  remainTime = Math.max(remainTime, 0); //fix -50
   const score = Math.round((1 - remainTime / (targetTime * 1000)) * 100);
   useImperativeHandle(ref, () => ({
     open() {
@@ -12,7 +14,7 @@ const ResultModal = forwardRef(function ResultModal(
     },
   }));
 
-  return (
+  return createPortal(
     <dialog ref={dialogRef} className="result-modal" onClose={onReset}>
       {remainTime <= 0 && <h2>You lost :(</h2>}
       {remainTime > 0 && <h2>You Score :{score}</h2>}
@@ -29,7 +31,8 @@ const ResultModal = forwardRef(function ResultModal(
       <form method="dialog" onSubmit={onReset}>
         <button>close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 
